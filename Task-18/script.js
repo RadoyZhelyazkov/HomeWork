5f6fed86d25ce70bec3b1be4b0347aebd2635258
Radoy Zhelyazkov <radoy.zhelyazkov@gmail.com>
2023-02-11 11:01:42 +0200

    Task-18

---
+++ Task-18/script.js
@@ -0,0 +1,10 @@
+var a = 0.1;
+var b = 0.7; 
+var c = 0.2; 
+var d = 0.8;
+
+console.log('statement : var a = 0.1; var b = 0.7; var c = 0.2; var d = 0.8; console.log(a + b); console.log(a + a + d); console.log(c + d); result:');
+
+console.log(a + b); 
+console.log(a + a + d); 
+console.log(c + d);
